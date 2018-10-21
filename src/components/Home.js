f480138f95ee074844cc5a46aab5b9ed787d0743
John Angel <j_angeldiaz@hotmail.com>
2018-10-21 09:57:43 -0500

    Code cleaning

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,8 @@
 import React, { Component,Fragment} from 'react';
 import { connect } from 'react-redux'
-import { Route } from 'react-router-dom'
 
-import {handleGetQuestions} from '../actions/questions'
-import {resetAuthedUser} from '../actions/authedUser' 
-
-import NavBar from './NavBar'
 import PollsUnanswered from './PollsUnanswered'
 import PollsAnswered from './PollsAnswered'
-import Leaderboard from './Leaderboard'
 import NewPoll from './NewPoll'
 
 class Home extends Component{
@@ -16,9 +10,7 @@ class Home extends Component{
     state = {
         displayUnAnsweredPolls: true
     }
-
     
-
     render() {
         return (            
             <Fragment>
