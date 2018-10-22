29294f9a848347289569f6ea8601ba9b42b727a7
John Angel <j_angeldiaz@hotmail.com>
2018-10-22 17:07:00 -0500

    Update NavLink font color when it's selected

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,38 +1,68 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
+import '../App.css'
 
-const NavBar = (props) => {
+class NavBar extends Component {
 
-    return (
+    state = {
+        classAnswered: 'notSelected',
+        classUnanswered: 'selected',
+        classLeaderboard: 'notSelected'
+    }
 
-        <nav className='nav'>
-            <ul>
-                <li>
-                    <NavLink to="/home" onClick={() => props.onAnsweredSelected()}
-                    >
-                        Answered
+    resetLinkClasses = () => {
+        this.setState({classAnswered: 'notSelected',classUnanswered:'notSelected',classLeaderboard:'notSelected'})
+
+    }
+
+    onAnsweredSelected = () => {
+        this.resetLinkClasses()
+        this.setState({classAnswered: 'selected'})
+        this.props.onAnsweredSelected()
+    }
+
+    onUnansweredSelected = () => {
+        this.resetLinkClasses()
+        this.setState({classUnanswered: 'selected'})
+        this.props.onUnansweredSelected()
+    }
+
+    onLeaderboardSelected = () => {
+        this.resetLinkClasses()
+        this.setState({classLeaderboard: 'selected'})
+    }
+
+    render(){
+        return (
+            <nav className='nav'>
+                <ul>
+                    <li>
+                        <NavLink to="/home" className={this.state.classAnswered} onClick={() => this.onAnsweredSelected()}
+                        >
+                            Answered
+                            </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/home" className={this.state.classUnanswered} onClick={() => this.onUnansweredSelected()}>
+                            Unanswered
+                            </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/leaderboard" className={this.state.classLeaderboard} onClick={() => this.onLeaderboardSelected()}>
+                            Leaderboard
+                            </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/" className="notSelected" onClick={() => this.props.onLogOutSelected()}>
+                            {this.props.user} - LogOut
                         </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/home" onClick={() => props.onUnansweredSelected()}>
-                        Unanswered
-                        </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/leaderboard">
-                        Leaderboard
-                        </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/" onClick={() => props.onLogOutSelected()}>
-                        {props.user} - LogOut
-                    </NavLink>
-                </li>
-            </ul>
-        </nav>
-    )
+                    </li>
+                </ul>
+            </nav>
+        )
 
+    }
 }
 
 const mapStateToProps = state => {
