3de33528aefc4840c491a598e5cb38d0d951f99b
John Angel <j_angeldiaz@hotmail.com>
2018-10-29 20:01:23 -0500

    Refactor NavBar to a Functional Stateless Component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,63 +1,44 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import '../App.css'
 
-class NavBar extends Component {
+const NavBar = (props) => {
 
-    onAnsweredSelected = () => {       
-        this.props.onAnsweredSelected()
-    }
-
-    onUnansweredSelected = () => {        
-        this.props.onUnansweredSelected()
-    }
-
-    onLeaderboardSelected = () => {        
-        this.props.onLeaderboardSelected()
-    }
-
-    onAddPollSelected = () => {
-        this.props.onAddPoll()
-    }
-
-
-    render(){
-        return (
-            <nav className='nav'>
-                <ul>
-                    <li>
-                        <NavLink to="/home" className={this.props.option === 'answered' ? 'selected' : 'notSelected'} onClick={() => this.props.onAnswered()}
-                        >
-                            Answered
+    return (
+        <nav className='nav'>
+            <ul>
+                <li>
+                    <NavLink to="/home" className={props.option === 'answered' ? 'selected' : 'notSelected'} onClick={() => props.onAnswered()}
+                    >
+                        Answered
                             </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/home" className={this.props.option === 'unanswered' ? 'selected' : 'notSelected'} onClick={() => this.props.onUnanswered()}>
-                            Unanswered
+                </li>
+                <li>
+                    <NavLink to="/home" className={props.option === 'unanswered' ? 'selected' : 'notSelected'} onClick={() => props.onUnanswered()}>
+                        Unanswered
                             </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/leaderboard" className={this.props.option === 'leaderboard' ? 'selected' : 'notSelected'} onClick={() => this.props.onLeaderboard()}>
-                            Leaderboard
+                </li>
+                <li>
+                    <NavLink to="/leaderboard" className={props.option === 'leaderboard' ? 'selected' : 'notSelected'} onClick={() => props.onLeaderboard()}>
+                        Leaderboard
                             </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/add" className={this.props.option === 'add' ? 'selected' : 'notSelected'} onClick={() => this.props.onAddPoll()}>
-                            Add Poll
+                </li>
+                <li>
+                    <NavLink to="/add" className={props.option === 'add' ? 'selected' : 'notSelected'} onClick={() => props.onAddPoll()}>
+                        Add Poll
                         </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/" className="notSelected" onClick={() => this.props.onLogOut()}>
-                            {this.props.user} - LogOut
+                </li>
+                <li>
+                    <NavLink to="/" className="notSelected" onClick={() => props.onLogOut()}>
+                        {props.user} - LogOut
                         </NavLink>
-                    </li>
-                </ul>
-            </nav>
-        )
-
-    }
+                </li>
+            </ul>
+        </nav>
+    )
 }
+
 
 const mapStateToProps = state => {
     return { 
